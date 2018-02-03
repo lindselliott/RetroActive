f1d6a010f9d833186f0ff77f6bfd747bfc507d49
Lindsay Elliott <lelliott@tribalscale.com>
2018-02-03 12:53:46 -0500

    LE/DY Requests and CSV
    Signed-off-by: Dorothy Yang <dyang@tribalscale.com>

---
+++ actionItem.py
@@ -0,0 +1,52 @@
+import json 
+
+class ActionItem:
+	def __init__(self, owner, assigned_to, start_date, end_date, task, completed):
+		self.owner = owner
+		self.assigned_to = assigned_to
+		self.start_date = start_date
+		self.end_date = end_date
+		self.task = task
+		self.completed = completed
+
+	@property
+	def owner(self):
+		return self.owner
+
+	@property
+	def assigned_to(self):
+		return self.assigned_to
+
+	@property
+	def start_date(self):
+		return self.start_date
+
+	@property
+	def end_date(self):
+		return self.end_date
+
+	@property
+	def task(self):
+		return self.task
+
+	@property
+	def completed(self):
+		return self.completed
+
+	def serialize(self):
+		return {
+			'owner': self.owner, 
+			'assigned_to': self.assigned_to,
+			'start_date': self.start_date,
+			'end_date': self.end_date,
+			'task': self.task,
+			'completed': self.completed
+		}
+
+	# def actions_items_export(self, file):
+	# 	output = csv.writer(file)
+	# 	output.writerow(self[0].keys())  # header row
+
+	# 	# import pdb; pdb.set_trace()
+	# 	for row in data:
+	# 	    output.writerow(row.values())
